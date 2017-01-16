ae9e4ed1548c0378df7714e7b7eafe9b551ee9fd
Eva Lieu <itsevalieu@gmail.com>
2017-01-15 18:17:32 -0800

    Got Buttons to show in the questionnaire container.

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -3,8 +3,12 @@ const path = require("path");
 
 module.exports =(app)=>{
 
-  app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../public/test.html'))
-  });
+	// app.get('/', (req, res)=>{
+	// 	res.sendFile(path.join(__dirname, '../public/test.html'))
+	// });
+
+	app.get("/", (req, res)=>{
+	    res.sendFile(path.join(__dirname, "../public/questionnaire.html"));
+	});
 
 };
